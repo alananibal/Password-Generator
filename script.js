a97eb6884f4899d157399077b86fee3e395ee00e
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-02-16 01:27:37 -0800

    1st.Structure

--- script.js
+++ script.js
@@ -12,3 +12,54 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// password criteria
+
+
+
+
+
+//
+//
+//
+
+// password lenght (atleast < 8, >128)
+var passwordLenght = window."I still down know the command"
+('how many characters would you like your password to have?');
+  // validade answer
+  if (passwordLenght >8 or <128) {
+    // command to storage this data into console('Data No.1:PassworldLenght')
+  }
+  // 
+else { 
+  window.prompt("Please choose an number between 8 and 128")
+  return passwordLenght();
+  // 
+ }
+
+
+
+// Characther Types: lower case, upper case, Numeric, and Or Special Characters
+// Lower Case
+var lowerCaseConfirm = window.confirm ("Would you like your passworld containing lower case?")
+  if (lowerCaseconfirm){
+    // code to add it to Passworld
+  }
+};else {}
+// Upper Case
+var upperCaseConfirm = window.confirm ("Would you like your passworld containing upper case?")
+  if (upperCaseconfirm){
+    // code to add it to Passworld
+  }else {}
+};
+// Numeric Numbers
+var numericNumbersConfirm = window.confirm ("Would you like your passworld containing Numeric Numbers?")
+  if (numericNumbersConfirm){
+    // code to add it to Passworld
+  }else {}
+};
+// Special Characters
+var specialCharactersConfirm = window.confirm ("Would you like your passworld containing special characters?")
+  if (specialCharacterCaseConfirm){
+    // code to add it to Passworld
+  }else {}
+};
